4c3f95db0eb9a5f6b0dab106652cfd500c0e33fe
petra <Petra Barus>
2017-09-09 04:54:43 +0700

    Create git utilities for checking git repo.

--- src/main/java/tdl/record/sourcecode/snapshot/helpers/GitHelper.java
+++ src/main/java/tdl/record/sourcecode/snapshot/helpers/GitHelper.java
@@ -1,12 +1,21 @@
 package tdl.record.sourcecode.snapshot.helpers;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Path;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryCache;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.util.FS;
+import org.eclipse.jgit.archive.ArchiveFormats;
 
 public class GitHelper {
+
+    public static String ARCHIVE_FORMAT_ZIP = "zip";
 
     public static boolean isGitDirectory(Path path) {
         File directory = path.toFile();
@@ -14,5 +23,15 @@ public class GitHelper {
         builder.addCeilingDirectory(directory);
         builder.findGitDir(directory);
         return builder.getGitDir() != null;
+    }
+
+    public static void exportGitArchive(Git git, OutputStream outputStream) throws GitAPIException, IOException {
+        ArchiveFormats.registerAll();
+        ObjectId tree = git.getRepository().resolve("master");
+        git.archive()
+                .setTree(tree)
+                .setFormat(ARCHIVE_FORMAT_ZIP)
+                .setOutputStream(outputStream)
+                .call();
     }
 }
--- src/test/java/tdl/record/sourcecode/snapshot/helpers/GitHelperTest.java
+++ src/test/java/tdl/record/sourcecode/snapshot/helpers/GitHelperTest.java
@@ -1,10 +1,16 @@
 package tdl.record.sourcecode.snapshot.helpers;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,23 +21,47 @@ public class GitHelperTest {
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
-    
+
     @Test
     public void isGitDirectoryShouldReturnTrue() throws IOException, GitAPIException {
         File directory = folder.newFolder();
         Git.init().setDirectory(directory).call();
-        
+
         assertTrue(GitHelper.isGitDirectory(directory.toPath()));
     }
-    
+
     @Test
     public void isGitDirectoryShouldReturnFalse() throws IOException {
         File directory = folder.newFolder();
-        
+
         assertFalse(GitHelper.isGitDirectory(directory.toPath()));
-        
+
         FileTestHelper.appendStringToFile(directory.toPath(), ".git/test", "Test");
-        
+
         assertFalse(GitHelper.isGitDirectory(directory.toPath()));
+    }
+
+    //@Test
+    public void exportGitArchive() throws IOException, GitAPIException {
+        File directory = folder.newFolder();
+        Git git = Git.init().setDirectory(directory).call();
+
+        FileTestHelper.appendStringToFile(directory.toPath(), "file1.txt", "Test");
+        FileTestHelper.appendStringToFile(directory.toPath(), "file2.txt", "Test");
+        FileTestHelper.appendStringToFile(directory.toPath(), "file3.txt", "Test");
+        git.commit().setAll(true).setMessage("Commit").call();
+
+        File archive = folder.newFile();
+        try (FileOutputStream fos = new FileOutputStream(archive)) {
+            GitHelper.exportGitArchive(git, fos);
+        }
+        assertTrue(archive.length() > 0);
+
+        ZipInputStream zis
+                = new ZipInputStream(new FileInputStream(archive));
+        ZipEntry ze = zis.getNextEntry();
+        assertNotNull(ze);
+        System.out.print(ze.getName());
+
     }
 }
