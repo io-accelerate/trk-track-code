fe57b1125bff75707b8a61ed2a5f3c398999f1b9
petra <Petra Barus>
2017-09-18 23:06:27 +0700

    Add start date to list command

--- src/main/java/tdl/record/sourcecode/ListCommand.java
+++ src/main/java/tdl/record/sourcecode/ListCommand.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.text.MessageFormat;
+import java.util.Date;
 import org.apache.commons.codec.binary.Hex;
 import tdl.record.sourcecode.snapshot.KeySnapshot;
 import tdl.record.sourcecode.snapshot.file.SnapshotFileSegment;
@@ -21,10 +22,11 @@ public class ListCommand extends Command {
     public void run() {
         File file = Paths.get(inputFilePath).toFile();
         try (SnapshotsFileReader reader = new SnapshotsFileReader(file)) {
+            Date start = new Date(reader.getFileHeader().getTimestamp());
             int index = 0;
             while (reader.hasNext()) {
                 SnapshotFileSegment segment = reader.next();
-                printSnapshot(segment, index);
+                printSnapshot(segment, index, start);
                 index++;
             }
         } catch (IOException ex) {
@@ -32,14 +34,13 @@ public class ListCommand extends Command {
         }
     }
 
-    private static void printSnapshot(SnapshotFileSegment segment, int index) {
+    private void printSnapshot(SnapshotFileSegment segment, int index, Date start) {
         String type = segment.getSnapshot() instanceof KeySnapshot ? "KEY" : "PATCH";
         long size = segment.size + SnapshotFileSegment.HEADER_SIZE;
         String checksum = Hex.encodeHexString(segment.checksum);
-
-        String infoLine = String.format("#%4d | time %4s | type %-5s | offset %5d | size %5d | checksum %40s",
-                index, segment.timestamp, type, segment.address, size, checksum);
+        String recorded = (new Date(start.getTime() + segment.timestamp)).toString();
+        String infoLine = String.format("#%4d | recorded %40s | time %4s | type %-5s | offset %5d | size %5d | checksum %40s",
+                index, recorded, segment.timestamp, type, segment.address, size, checksum);
         System.out.println(infoLine);
-
     }
 }
