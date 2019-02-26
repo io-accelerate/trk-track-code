c1d7302bb9a7116cdb77a488a8e62f43ba82ee48
neomatrix369 <sadhak001@gmail.com>
2019-02-26 22:44:43 +0000

    Removed unused interfaces and implementations to the underlying methods via them

--- src/main/java/tdl/record/sourcecode/content/CombinedFileFilter.java
+++ src/main/java/tdl/record/sourcecode/content/CombinedFileFilter.java
@@ -1,13 +1,12 @@
 package tdl.record.sourcecode.content;
 
-import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 import tdl.record.sourcecode.snapshot.helpers.ExcludeGitDirectoryFileFilter;
 
 import java.io.File;
 import java.io.FileFilter;
 
-public class CombinedFileFilter implements FileFilter, IOFileFilter {
+public class CombinedFileFilter implements FileFilter {
     private final ExcludeGitDirectoryFileFilter excludeGitDirectoryFileFilter;
     private final WildcardFileFilter ignoredFilesFilter;
     private MinimumFileSizeFilter minimumFileSizeFilter;
@@ -25,12 +24,5 @@ public class CombinedFileFilter implements FileFilter, IOFileFilter {
         return excludeGitDirectoryFileFilter.accept(pathname) &&
                 !ignoredFilesFilter.accept(pathname) &&
                 minimumFileSizeFilter.accept(pathname);
-    }
-
-    @Override
-    public boolean accept(File dir, String name) {
-        return excludeGitDirectoryFileFilter.accept(dir, name) &&
-                !ignoredFilesFilter.accept(dir, name) &&
-                minimumFileSizeFilter.accept(dir, name);
     }
 }
--- src/main/java/tdl/record/sourcecode/content/MinimumFileSizeFilter.java
+++ src/main/java/tdl/record/sourcecode/content/MinimumFileSizeFilter.java
@@ -1,11 +1,9 @@
 package tdl.record.sourcecode.content;
 
-import org.apache.commons.io.filefilter.IOFileFilter;
-
 import java.io.File;
 import java.io.FileFilter;
 
-public class MinimumFileSizeFilter implements FileFilter, IOFileFilter {
+public class MinimumFileSizeFilter implements FileFilter {
 
     private final long minimumFileSize;
 
@@ -16,11 +14,5 @@ public class MinimumFileSizeFilter implements FileFilter, IOFileFilter {
     @Override
     public boolean accept(File f) {
         return f.length() <= minimumFileSize;
-    }
-
-    @Override
-    public boolean accept(File dir, String name) {
-        File file = new File(dir.toString() + File.separator + name);
-        return file.length() <= minimumFileSize;
     }
 }
